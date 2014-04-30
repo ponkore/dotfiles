d2c104c95a554fbde3533ffd366104624f19c5c2
Ponkore <ponkore@gmail.com>
2014-05-01 01:53:02 +0900

    .lein/profiles.clj: cider-nrepl added bin/lein: lein command version up.

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [ [lein-pprint "1.1.1"]
-                   [codox "0.6.4"]
-                   ]}}
+{:user {:plugins [[lein-pprint "1.1.1"]
+                  [codox "0.6.4"]
+                  [cider/cider-nrepl "0.6.1-SNAPSHOT"]]}}
