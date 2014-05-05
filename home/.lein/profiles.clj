1e2415d1b211a21ee36894f33993cbbb5abad03c
Ponkore <ponkore@gmail.com>
2014-05-05 22:20:09 +0900

    leiningen .lein/profiles.clj modified(for debugging)

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,3 +1,33 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [codox "0.6.4"]
-                  [cider/cider-nrepl "0.6.1-SNAPSHOT"]]}}
+;; http://z.caudate.me/give-your-clojure-workflow-more-flow/
+{:user
+ {:plugins [[lein-pprint "1.1.1"]
+            [lein-midje "3.1.3"]
+            [lein-midje-doc "0.0.21"]
+            [jonase/eastwood "0.1.2"]
+            [codox "0.6.6"]
+            [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+  :dependencies [[spyscope "0.1.4"]
+                 ;; [im.chit/vinyasa "0.2.0"]
+                 [io.aviso/pretty "0.1.8"]]
+  :injections [(require 'spyscope.core
+                        ;; 'vinyasa.inject
+                        'io.aviso.repl
+                        'clojure.repl
+                        'clojure.main)
+               ;; (vinyasa.inject/inject 'clojure.core
+               ;;                        '[[vinyasa.inject [inject inject]]
+               ;;                          [vinyasa.pull [pull pull]]
+               ;;                          ;; [vinyasa.lein [lein lein]]
+               ;;                          [clojure.repl apropos dir doc find-doc source
+               ;;                           [root-cause cause]]
+               ;;                          [clojure.tools.namespace.repl [refresh refresh]]
+               ;;                          [clojure.pprint [pprint >pprint]]
+               ;;                          [io.aviso.binary [write-binary >bin]]])
+               (alter-var-root #'clojure.main/repl-caught
+                               (constantly @#'io.aviso.repl/pretty-pst))
+               (alter-var-root #'clojure.repl/pst
+                               (constantly @#'io.aviso.repl/pretty-pst))]
+  }
+ :repl-options {
+    :nrepl-middleware [io.aviso.nrepl/pretty-middleware]
+ }}
