a4781d6ca4653eb865d074b1464b24a55d697916
ponkore <ponkore@gmail.com>
2017-12-05 23:52:00 +0900

    migrate vinyasa -> lucid

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,27 +1,22 @@
 ;; http://z.caudate.me/give-your-clojure-workflow-more-flow/
 {:user
  {:plugins [[lein-pprint "1.1.2"]
-            [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]]}
+            [lein-ancient "0.6.14" :exclusions [org.clojure/clojure]]]}
  :repl
  {:dependencies [[clj-time "0.11.0"]
                  [spyscope "0.1.5" :exclusions [clj-time]]
                  [org.clojure/tools.namespace "0.2.10"]
                  [org.clojure/tools.nrepl "0.2.12"]
-                 [leiningen #=(leiningen.core.main/leiningen-version)]
-                 [im.chit/hara "2.2.11"]
-                 [im.chit/vinyasa "0.4.2" :exclusions [im.chit/hara.namespace.import]]]
+                 [im.chit/lucid.mind "1.3.13"]
+                 [im.chit/lucid.core.inject "1.3.13"]]
   :injections [(require 'spyscope.core)
-               (require '[vinyasa.inject :as inject])
-               (inject/in  ;; the default injected namespace is `.`
+               (require '[lucid.core.inject :as inject])
+               (inject/in ;; the default injected namespace is `.`
                 ;; note that `:refer, :all and :exclude can be used
-                [vinyasa.inject :refer [inject [in inject-in]]]
-                [vinyasa.lein :exclude [*project*]]
-                ;; same as [cemerick.pomegranate :refer [add-classpath get-classpath resources]]
-                [cemerick.pomegranate add-classpath get-classpath resources]
+                [lucid.core.inject :refer [inject [in inject-in]]]
                 ;; inject into clojure.core
                 clojure.core
-                [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
-                [vinyasa.lein lein]
+                [lucid.mind .> .? .* .% .%> .& .>ns .>var]
                 ;; inject into clojure.core with prefix
                 clojure.core >
                 [clojure.pprint pprint]
