6e83ed774ab6d34d11987f12f90ab286e46baba8
ponkore <ponkore@gmail.com>
2019-07-20 02:11:00 +0900

    lein verup 2.8.3->2.9.1, java11

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -3,5 +3,8 @@
             [lein-ancient "0.6.14" :exclusions [org.clojure/clojure]]]}
  :repl
  {:dependencies [[clj-time "0.15.0"]
-                 [spyscope "0.1.6" :exclusions [clj-time]]]
-  :injections [(require 'spyscope.core)]}}
+                 [org.clojure/core.rrb-vector "0.0.14"]
+                 [fipp "0.6.18" :exclusions [org.clojure/clojure org.clojure/core.rrb-vector]]
+                 [spyscope "0.1.6" :exclusions [clj-time org.clojure/core.rrb-vector]]]
+  :injections [(require 'spyscope.core)]
+ :signing {:gpg-key "F1933B1EEFAD412D"}}}
