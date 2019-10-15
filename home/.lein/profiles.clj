125be316b544e03e3f6fd5fb97bd67ddec1c591f
ponkore <ponkore@gmail.com>
2019-10-15 20:36:34 +0900

    update deps: core-rrb-vector, fipp

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -3,8 +3,8 @@
             [lein-ancient "0.6.14" :exclusions [org.clojure/clojure]]]}
  :repl
  {:dependencies [[clj-time "0.15.0"]
-                 [org.clojure/core.rrb-vector "0.0.14"]
-                 [fipp "0.6.18" :exclusions [org.clojure/clojure org.clojure/core.rrb-vector]]
+                 [org.clojure/core.rrb-vector "0.1.0"]
+                 [fipp "0.6.21" :exclusions [org.clojure/clojure org.clojure/core.rrb-vector]]
                  [spyscope "0.1.6" :exclusions [clj-time org.clojure/core.rrb-vector]]]
   :injections [(require 'spyscope.core)]
  :signing {:gpg-key "F1933B1EEFAD412D"}}}
