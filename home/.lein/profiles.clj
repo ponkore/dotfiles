989bd4d52b83131e933fe591f0751bc204d80a0a
ponkore <ponkore@gmail.com>
2023-04-15 23:59:09 +0900

    minor fix

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -2,9 +2,11 @@
  {:plugins [[lein-pprint "1.3.2"]
             [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]]}
  :repl
- {;; :dependencies [[clj-time "0.15.2"]
+ {:plugins [[cider/cider-nrepl "0.28.5"]
+            [mx.cider/enrich-classpath "1.9.0"]]
+  ;; :dependencies [[clj-time "0.15.2"]
   ;;                [org.clojure/core.rrb-vector "0.1.0"]
   ;;                [fipp "0.6.23" :exclusions [org.clojure/clojure org.clojure/core.rrb-vector]]
   ;;                [spyscope "0.1.6" :exclusions [clj-time org.clojure/core.rrb-vector]]]
   ;; :injections [(require 'spyscope.core)]
- :signing {:gpg-key "F1933B1EEFAD412D"}}}
+  :signing {:gpg-key "F1933B1EEFAD412D"}}}
